8daa67e45ded7829bd4713cab11022f79dea45d6
Mrahel1993 <mrahel1993@gmail.com>
2024-12-04 22:47:56 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // إعداد الـ API Token الخاص بـ Telegram
-const token = 'YOUR_API_TOKEN';
+const token = '7859625373:AAEFlMbm3Sfagj4S9rx5ixbfqItE1jNpDos';
 const bot = new TelegramBot(token);
 
 // إعداد Webhook
-const webhookUrl = 'https://your-server.com/bot'; // استبدل بـ URL الخاص بك
+const webhookUrl = 'https://trygaz.onrender.com'; // استبدل بـ URL الخاص بك
 bot.setWebHook(`${webhookUrl}/bot`);
 
 module.exports = bot;
