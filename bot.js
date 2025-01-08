7d47467d545436a9ccabff537ef41e955720778e
Mrahel1993 <mrahel1993@gmail.com>
2025-01-09 02:04:17 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
 const { sendMessageToAdmins } = require('./utils');
 
-const token = process.env.TELEGRAM_BOT_TOKEN || 'your-token';
+const token = process.env.TELEGRAM_BOT_TOKEN || '7742968603:AAFD-02grJl4Kt2V9b6Z-AxaCbwopEx_zZU';
 const bot = new TelegramBot(token, { polling: false });
 
-const webhookUrl = process.env.WEBHOOK_URL || 'https://example.com';
+const webhookUrl = process.env.WEBHOOK_URL || 'https://trygaz.onrender.com';
 bot.setWebHook(`${webhookUrl}/bot${token}`);
 
 module.exports = bot;
